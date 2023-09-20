db14d592681db29ffe9100af7a3db99e10d79e86
zied-snoussi <ziedsnoussi.tn@gmail.com>
2023-09-20 15:18:49 +0100

    make tests

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -2,29 +2,29 @@ import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
 
 describe('Home', () => {
-    it('should have Docs text', () => {
-        render(<Home />) // ARRANGE
+  it('should have "Welcome to My Next.js Starter Template" text', () => {
+    render(<Home />) // ARRANGE
 
-        const myElem = screen.getByText('Docs') // ACT
+    const myElem = screen.getByText('Welcome to My Next.js Starter Template') // ACT
 
-        expect(myElem).toBeInTheDocument() // ASSERT
-    })
+    expect(myElem).toBeInTheDocument() // ASSERT
+  })
 
-    it('should contain the text "information"', () => {
-        render(<Home />) // ARRANGE
+  it('should contain the text "This is the home page of your starter template."', () => {
+    render(<Home />) // ARRANGE
 
-        const myElem = screen.getByText(/information/i) // ACT
+    const myElem = screen.getByText('This is the home page of your starter template.') // ACT
 
-        expect(myElem).toBeInTheDocument() // ASSERT
-    })
+    expect(myElem).toBeInTheDocument() // ASSERT
+  })
 
-    it('should have a heading', () => {
-        render(<Home />) // ARRANGE
+  it('should have a heading with the text "Fast Performance"', () => {
+    render(<Home />) // ARRANGE
 
-        const myElem = screen.getByRole('heading', {
-            name: 'Learn'
-        }) // ACT
+    const myElem = screen.getByRole('heading', {
+      name: 'Fast Performance'
+    }) // ACT
 
-        expect(myElem).toBeInTheDocument() // ASSERT
-    })
+    expect(myElem).toBeInTheDocument() // ASSERT
+  })
 })
