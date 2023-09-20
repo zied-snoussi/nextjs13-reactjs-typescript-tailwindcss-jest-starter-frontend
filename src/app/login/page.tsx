7b1f5c9a1cf318f4fef28c2771137075de3d93fc
zied-snoussi <ziedsnoussi.tn@gmail.com>
2023-09-20 15:01:51 +0100

    customise pages and add link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="max-w-md p-8 space-y-4 bg-white shadow-lg rounded-lg">
+        <h1 className="text-3xl font-bold">Login</h1>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-600 text-sm font-medium mb-1"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-500"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-600 text-sm font-medium mb-1"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-blue-500"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
-import Image from 'next/image'
-
 export default function Home() {
+  // Example content data
+  const features = [
+    {
+      icon: '🚀',
+      title: 'Fast Performance',
+      description: 'Next.js provides fast server rendering and optimized production builds.',
+    },
+    {
+      icon: '🛠️',
+      title: 'Easy Development',
+      description: 'Develop with ease using Next.js and Tailwind CSS for styling.',
+    },
+    {
+      icon: '📚',
+      title: 'Interactive Learning',
+      description: 'Learn and practice Next.js with interactive quizzes.',
+    },
+  ];
+
   return (
-    <div>
-      <h1>Learn</h1>
-      <p>Learn about Next.js in an interactive course with quizzes!</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md p-8 space-y-4 bg-white shadow-lg rounded-lg">
+        <h1 className="text-3xl font-bold">Welcome to My Next.js Starter Template</h1>
+        <p className="text-gray-600">This is the home page of your starter template.</p>
+
+        <div className="space-y-2">
+          {features.map((feature, index) => (
+            <div key={index} className="flex space-x-2">
+              <span className="text-2xl">{feature.icon}</span>
+              <div>
+                <h2 className="text-lg font-semibold">{feature.title}</h2>
+                <p className="text-gray-600">{feature.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
